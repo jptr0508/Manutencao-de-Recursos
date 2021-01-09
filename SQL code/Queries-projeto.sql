803d6aac4d89912f9da2c8687ba262a2751dc45d
jptr0508 <73311583+jptr0508@users.noreply.github.com>
2021-01-09 23:46:20 +0000

    Add files via upload

---
+++ SQL code/Queries-projeto.sql
@@ -0,0 +1,20 @@
+select consumo*tempo_on*7 as 'Consumo', stud_name
+from eletrodomestico;
+
+select *
+from utilizador;
+
+select * 
+from utilizador
+where user_sexo = 'F';
+
+select *
+from consumo
+where tipo = 'eletricidade';
+
+select *
+from consumo
+where tipo = 'gás';
+
+select user_nome as 'Nome', user_idade as Birthdate, timestampdiff(YEAR,user_idade,sysdate()) as Age 
+from utilizador;
